cba97f7905ee5b57fa74c03d595039e898a3080c
christian valzado <christianvalzado2@gmail.com>
2024-10-09 00:50:20 -0700

    docs:adding UniversityDict.py

---
+++ UniversityDict.py
@@ -0,0 +1,49 @@
+university_dict1 = {
+    "ID" : "1",
+    "Name" : "University of the Philippines",
+    "Location" : "Quezon City",
+    "Established Year" : "1908",
+    "Type" : "Public",
+    "Website" : "www.up.edu.ph"
+}
+print(university_dict1)
+
+university_dict2 = {
+    "ID" : "2",
+    "Name" : "Ateneo de Manila University",
+    "Location" : "Quezon City",
+    "Established Year" : "1859",
+    "Type" : "Private",
+    "Website" : "www.ateneo.edu"
+}
+print(university_dict2)
+
+university_dict3 = {
+    "ID" : "3",
+    "Name" : "De La Salle University",
+    "Location" : "Manila",
+    "Established Year" : "1911",
+    "Type" : "Private",
+    "Website" : "www.dlsu.edu.ph"
+}
+print(university_dict3)
+
+university_dict4 = {
+    "ID" : "4",
+    "Name" : "University of Santo Tomas",
+    "Location" : "Manila",
+    "Established Year" : "1611",
+    "Type" : "Private",
+    "Website" : "www.ust.edu.ph"
+}
+print(university_dict4)
+
+university_dict5 = {
+    "ID" : "5",
+    "Name" : "Polytechnic University of the Philippines	",
+    "Location" : "Manila",
+    "Established Year" : "1904 ",
+    "Type" : "Public",
+    "Website" : "www.pup.edu.ph"
+}
+print(university_dict5)
