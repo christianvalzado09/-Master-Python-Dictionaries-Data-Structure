fc1d21f3910bfde855b8e4f940e7684fb3d30efb
christian valzado <christianvalzado2@gmail.com>
2024-10-09 00:41:44 -0700

    docs:adding EmployeeDict.py

---
+++ EmployeeDict.py
@@ -0,0 +1,44 @@
+employee_dict1 = {
+    "ID" : "1",
+    "Name" : "John Doe",
+    "Department" : "Sales",
+    "Age" : "30",
+    "Email" : "john.doe@company.com"
+}
+print(employee_dict1)
+
+employee_dict2 = {
+    "ID" : "2",
+    "Name" : "Jane Smith",
+    "Department" : "Human Resources",
+    "Age" : "25",
+    "Email" : "jane.smith@company.com"
+}
+print(employee_dict2)
+
+employee_dict3 = {
+    "ID" : "3",
+    "Name" : "Mark Johnson",
+    "Department" : "IT",
+    "Age" : "40",
+    "Email" : "mark.johnson@company.com"
+}
+print(employee_dict3)
+
+employee_dict4 = {
+    "ID" : "4",
+    "Name" : "Lisa Wong",
+    "Department" : "Marketing",
+    "Age" : "28",
+    "Email" : "lisa.wong@company.com"
+}
+print(employee_dict4)
+
+employee_dict5 = {
+    "ID" : "5",
+    "Name" : "Paul McDonald",
+    "Department" : "Finance",
+    "Age" : "28",
+    "Email" : "paul.mcdonald@company.com"
+}
+print(employee_dict5)
