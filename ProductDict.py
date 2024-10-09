751018eb5c65691605d0a853a3778a45f717c5ab
christian valzado <christianvalzado2@gmail.com>
2024-10-09 00:47:04 -0700

    docs:adding ProductDict.py

---
+++ ProductDict.py
@@ -0,0 +1,49 @@
+product_dict1 = {
+    "ID": "1",
+    "Name": "Laptop",
+    "Category": "Electronics",
+    "Price": "750",
+    "Stock": "50",
+    "Supplier Email": "supplier1@gmail.com"
+}
+print(product_dict1)
+
+product_dict2 = {
+    "ID": "2",
+    "Name": "Desk Chair",
+    "Category": "Furniture",
+    "Price": "100",
+    "Stock": "200",
+    "Supplier Email": "supplier2@gmail.com"
+}
+print(product_dict2)
+
+product_dict3 = {
+    "ID": "3",
+    "Name": "Smartwatch",
+    "Category": "Electronics",
+    "Price": "200",
+    "Stock": "150",
+    "Supplier Email": "supplier3@gmail.com"
+}
+print(product_dict3)
+
+product_dict4 = {
+    "ID": "4",
+    "Name": "Notebook",
+    "Category": "Stationery",
+    "Price": "5",
+    "Stock": "500",
+    "Supplier Email": "supplier4@gmail.com"
+}
+print(product_dict4)
+
+product_dict5 = {
+    "ID": "5",
+    "Name": "Running Shoes",
+    "Category": "Apparel",
+    "Price": "80",
+    "Stock": "100",
+    "Supplier Email": "supplier5@gmail.com"
+}
+print(product_dict5)
